bce65ca50b392e37afc32e72067c325431256703
pkkarn <=>
2020-08-25 14:47:10 +0530

    Search Sentivity Fixed

--- movie/filters.py
+++ movie/filters.py
@@ -1,7 +1,10 @@
 import django_filters
 from .models import *
+from django_filters import CharFilter
 
-class MovieFilter(django_filters.FilterSet):
+class MovieFilter(django_filters.FilterSet):  # Search Form
+    movieName = CharFilter(field_name='movieName', lookup_expr='icontains')
+
     class Meta:
         model = Movie
-        fields = ['movieName','movieDescription']
+        fields = ['movieName']
